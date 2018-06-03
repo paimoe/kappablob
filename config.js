f3502d022dd2e222a258b6a76d8b264aac7a0a55
paimoe <me@paimoe.com>
2018-06-03 12:29:10 +0800

    Add CORS_ORIGIN to config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Config or whatevs
-exports.TWITCH_USER = 'buddhasdietitian'; // twitch username
-exports.TWITCH_AUTH = 'oauth:ae7w18ikiomu37c85487ix55odyvlb'; // oauth:xxx
+exports.TWITCH_USER = ''; // twitch username
+exports.TWITCH_AUTH = ''; // oauth:xxx https://twitchapps.com/tmi/)
+exports.CORS_ORIGIN = '';
